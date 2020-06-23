b2d67f60b89defa69be6fac3cb2b726b8eed3c0e
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2020-06-23 16:01:27 -0300

    GET and POST Implementation

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTotal = this.transactions
+      .map((transaction: Transaction) =>
+        transaction.type === 'income'
+          ? transaction.value
+          : 0,
+      )
+      .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+    const outcomeTotal = this.transactions
+      .map((transaction: Transaction) =>
+        transaction.type === 'outcome'
+          ? transaction.value
+          : 0,
+      )
+      .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+    const balance: Balance = {
+      income: incomeTotal,
+      outcome: outcomeTotal,
+      total: incomeTotal - outcomeTotal,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type, }: RequestDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
